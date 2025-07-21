eb9eb15f4b34a2b6779e2ad70398a76444e29560
Shizuo0 <paulosvtatibana@gmail.com>
2025-07-21 00:56:38 +0000

    Fix db migration

--- db/migrate/20250721003101_fix_timestamps_on_users.rb
+++
@@ -1,6 +0,0 @@
-class FixTimestampsOnUsers < ActiveRecord::Migration[8.0]
-  def change
-    change_column_default :users, :created_at, from: -> { "CURRENT_TIMESTAMP" }, to: nil
-    change_column_default :users, :updated_at, from: -> { "CURRENT_TIMESTAMP" }, to: nil
-  end
-end
