ead7927723407d115083623e2edf5e5009742708
Shizuo0 <paulosvtatibana@gmail.com>
2025-07-21 17:17:45 +0000

    Fix simplecov build on devcontainer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,10 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  coverage_dir ".devcontainer/coverage"
+  add_filter "/spec/"
+end
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
